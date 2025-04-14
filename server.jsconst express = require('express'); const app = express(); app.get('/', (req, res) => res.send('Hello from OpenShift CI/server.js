b2dc6cc16a088186720e5029073300fe4ce74fa3
Ashhad Ali <54775497+ashhadali10@users.noreply.github.com>
2025-04-15 01:58:06 +0500

    Delete server.jsconst express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Hello from OpenShift CI directory

--- server.jsconst express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Hello from OpenShift CI/server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello from OpenShift CI/CD!'));
-app.listen(8080, () => console.log('Server running on port 8080'));
