848d6a9ae59b46436e4364edd9c06109a0df5372
Ashhad Ali <54775497+ashhadali10@users.noreply.github.com>
2025-04-18 22:57:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from OpenShiftaaaaaa CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from updateupdateeeeeeeeeeeeeeeeeeee CI/CD!'));
 app.listen(8080, () => console.log('Server running on port 8080'));
